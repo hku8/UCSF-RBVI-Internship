3f4e8fca51ddfb86d478ab8d7f3e60778903ca85
suzuryu9000 <suzuryu9000@gmail.com>
2017-06-16 19:11:44 -0700

    dictionary + start of atom sec

--- Sample/src/io.py
+++ Sample/src/io.py
@@ -1,7 +1,10 @@
 # vim: set expandtab shiftwidth=4 softtabstop=4:
 
+import sys
+print (sys.version + "\n" + sys.executable)
+print ("-"*50+ "\n")
 
-def open_xyz(session, stream, name):
+def open_mol2(session, stream, name):
     structures = []
     atoms = 0
     bonds = 0
@@ -12,56 +15,89 @@ def open_xyz(session, stream, name):
         structures.append(s)
         atoms += s.num_atoms
         bonds += s.num_bonds
-    status = ("Opened XYZ file containing %d structures (%d atoms, %d bonds)" %
+    status = ("Opened mol2 file containing {} structures ({} atoms, {} bonds)".format
               (len(structures), atoms, bonds))
     return structures, status
 
 
 def _read_block(session, stream):
-    # First line should be an integer count of the number of
-    # atoms in the block.  Each block gets turned into an
-    # AtomicStructure instance.
-    count_line = stream.readline()
-    if not count_line:
-        return None
-    try:
-        count = int(count_line)
-    except ValueError:
-        # XXX: Should emit an error message
-        return None
-    from chimerax.core.atomic import AtomicStructure
-    s = AtomicStructure(session)
+    """test docstring"""
+    # First section should be commented out
+    # Second section: "@<TRIPOS>MOLECULE"
+    # Third section: "@<TRIPOS>ATOM"
+    # Fourth section: "@<TRIPOS>BOND"
+    # Fifth section: "@<TRIPOS>SUBSTRUCTURE"
 
-    # Next line is a comment line
-    s.comment = stream.readline().strip()
+    import ast
 
-    # There should be "count" lines of atoms.
-    from numpy import array, float64
-    residue = s.new_residue("UNK", 'A', 1)
-    element_count = {}
-    for n in range(count):
-        atom_line = stream.readline()
-        if not atom_line:
-            # XXX: Should emit an error message
-            return None
-        parts = atom_line.split()
-        if len(parts) != 4:
-            # XXX: Should emit an error message
-            return None
-        # Extract available data
-        element = parts[0]
-        xyz = [float(v) for v in parts[1:]]
 
-        # Convert to required initializers
-        # XXX: May need to convert element to usable form
-        n = element_count.get(element, 0) + 1
-        name = element + str(n)
-        element_count[element] = n
 
-        # Create atom
-        atom = s.new_atom(name, element)
-        atom.coord = array(xyz, dtype=float64)
-        residue.add_atom(atom)
-    s.connect_structure([residue], [residue], [], [])
-    s.new_atoms()   # tell structure it needs to update
-    return s
+    def pl(num_of_lines=None):
+        """for testing purposes only. delete later"""
+        if num_of_lines is None:
+            num_of_lines = 1
+        print("\n" + ("=")*30 + "\nbeginning test print...\n" + ("=")*30 + "\n")
+        for _ in range(0, num_of_lines):
+            print(stream.readline().strip())
+        print("\n" + ("=")*30 + "\nending test print\n"+("=")*30 + "\n")
+
+
+
+
+    comment = stream.readline()
+
+    property_dict = {}
+
+    while comment[0] == "#":
+        line = comment.replace("#", "")
+        parts = line.split(":")
+        parts = [item.strip() for item in parts]
+        if ":" not in line:
+            for i in range(len(line), 1, -1):
+                if line[i-1] == " ":
+                    property_dict[line[:i].strip()] = line[i:].strip()
+                    break
+
+        else:
+            try:
+                property_dict[str(parts[0])] = ast.literal_eval(parts[1])
+
+            except ValueError:
+                property_dict[str(parts[0])] = str(parts[1])
+
+        comment = stream.readline()
+
+    for key, value in property_dict.items():
+        print(key, ":", value)
+
+    # Property Dictionary should be completed at this point
+
+    ###test print. Delete later
+    print()
+    for i in property_dict:
+        val = property_dict[i]
+        print(str(val) + " : " + str(type(val)))
+
+
+    while True:
+        if "@<TRIPOS>ATOM" in stream.readline():
+            atom_line = stream.readline()
+            parts = atom_line.split()
+            print (parts)
+
+
+
+    
+
+
+
+
+
+
+# _read_block(None, open("ras.mol2", "r"))
+_read_block(None, open("ras(short_version).mol2", "r"))
+
+
+
+
+# note to self: try to ask if you can do a pip install package that would not disappear
